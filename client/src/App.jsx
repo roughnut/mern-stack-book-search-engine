8fe633a79920a514c4b3b4193b9381bbd727e150
roughnut <jeremy@writever.com.au>
2024-07-27 14:48:10 +1000

    update App.jxs to include Apollo Provider and Apollo Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,47 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import { Outlet } from "react-router-dom";
+// import Appollo functionality
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+// import setContext function
+import { setContext } from "@apollo/client/link/context";
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+
+// Create an HTTP link to the GraphQL server
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Create an auth link to add the token to the header of each request
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Create the Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    // wrap the React app in the Appollo client
+    // permits the components to interact with GraphQL server
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
