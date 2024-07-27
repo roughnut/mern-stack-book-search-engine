7c1d19d66783e35b8413a90dbe5f191415d3ef3c
roughnut <jeremy@writever.com.au>
2024-07-27 12:58:35 +1000

    refactor server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,46 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+// import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+// import authMiddleware to handle JWT authentication
+const { authMiddleware } = require("./utils/auth");
+// import GraphQL typedefs and resolvers that define schema and queries/mutations for ApolloServer
+const { typeDefs, resolvers } = require("./schemas");
 
-const app = express();
+const db = require("./config/connection");
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+// set up ApolloServer with GraphQL schema and JWT authentication context
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+const startApolloServer = async () => {
+  // start the server
+  await server.start();
+  // connect ApolloServer to the Express app. 
+  // the applyMiddleware method includes a /graphql endpoint
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // server.graphqlPath is the default endpoint /graphql from .applyMiddleware
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+startApolloServer();
