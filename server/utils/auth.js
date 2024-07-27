b683b72548e32c38aea8b2f4bbd148ad57c5cb74
roughnut <jeremy@writever.com.au>
2024-07-27 14:30:19 +1000

    update server/utils/auth.js to include graphQL context

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,35 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+    
+    // Authorization: Bearer <token>
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      // split Bearer & the token, pop "Bearer" and trim token
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      // return the req object unchanged so unauthenticated request can be handled as necessary
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
+      // verify the token, destructure the payload and add data to the req.user object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      console.log("Invalid token");
     }
 
-    // send to next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
