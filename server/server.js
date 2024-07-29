e7674ba30413349be8334ca450191e08c4c242f3
roughnut <jeremy@writever.com.au>
2024-07-29 18:29:29 +1000

    server.js syntax error fix

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 }
 
 const startApolloServer = async () => {
